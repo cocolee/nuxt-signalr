7806b3cb37eaf122e29f198644726172659a2141
Rifat Caner ANILTURK <coco@Rifats-MacBook-Pro.local>
2021-11-30 00:28:03 +0300

    improve connect hub

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    Works!
+    <nuxt-link to="/about">About</nuxt-link>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async created () {
+    await this.$signalRHub.StartHub()
+    await this.$signalRHub.JoinHubGroup([
+      'JoinMarketHubLiteGroupAsync',
+      'marketSummary'
+    ])
+  },
+  async mounted () {
+    console.log('mounred =========> ', await this.$signalRHubConnection)
+    await this.$signalRHubConnection.off('marketSummary')
+    setTimeout(() => {
+      this.$signalRHubConnection.on('marketSummary', (summary) => {
+        console.log('summary =============> ', summary)
+      })
+    }, 500);
+    // console.log('asdasdasd=>>>', await this.$signalRHubConnection)
+    // console.log('this.$signalRHub.Connection.state', this.$signalRHub.Connection.state)
+    // console.log('this.$signalRHub.Connection', await this.$signalRHub.ConnectionState)
+    // await this.$signalRHub.addHubListener('marketSummary', 'summary').then((res) => {
+    //   console.log('index res', res)
+    // })
+  },
+}
 </script>
